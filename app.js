02cfa8ce59816dc28f9f629d41d50facc455a0ec
슬 <seul@I-got-no-energy.local>
2024-09-04 21:03:47 +0900

    :sparkles:feat: 클릭으로 그리기 / 브러시 두께 / 팔레트 추가

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const canvas = document.querySelector('canvas');
+const modeBtn = document.getElementById("mode-btn");
 
+// const colorOptions = document.getElementsByClassName('color-option');
+// => 이렇게 생성한 colorOptions는 ArrayLike 객체(O), Array(X)이므로 forEach로 접근할 수 없음.
+const colorOptions = Array.from(document.getElementsByClassName('color-option'));
+// => 따라서 Array.from을 사용해 배열로 만들어줌
+
+const currentColor = document.querySelector('.current-color');
+
+const color = document.getElementById('color');
+const lineWidth = document.getElementById('line-width');
+const canvas = document.querySelector('canvas');
 // context : 페인트 브러쉬 => 축약해서 ctx로 씀
 const ctx = canvas.getContext('2d'); //WebGL은 3D 요소
 
@@ -7,64 +17,52 @@ const ctx = canvas.getContext('2d'); //WebGL은 3D 요소
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 1;
+ctx.lineWidth = lineWidth.value;
 
+let isPainting = false;
 
-const colors = {
-    sunset : [
-        "#3E7EAC",
-        "#F2F3F5",
-        "#FBACA4", 
-        "#FFDCD4",
-        "#D5756D"
-    ],
-
-    spring : [
-        "#F3F6EB",
-        "#FFCE55",
-        "#DBF68F",
-        "#92C3A5",
-        "#568366"
-    ],
-
-    ocean : [
-        "#2F4156",
-        "#567C8D",
-        "#C8D9E6",
-        "#F5EFEB",
-        "#FFFFFF"
-    ]
-
-}
-
-// 저장될 테마
-let selectTheme;
-
-// 테마 랜덤 설정 함수
-function randomTheme() {
-    const themeKey = Object.keys(colors);
-    const randomKey = themeKey[Math.trunc(Math.random() * themeKey.length)];
-    selectTheme = colors[randomKey];
-}
-
-// 테마가 지정되지 않았을 때(= 첫 클릭시) 테마 랜덤 설정 함수 실행
-canvas.addEventListener('click', (event) => {
-    if (!selectTheme) {
-        randomTheme();
+function onMove(event) {
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+        // isPainting이 true이면(= mousedown 상태이면) 선 그리기
     }
-    onClick(event);
-});
-
-function onClick(event) {
     ctx.beginPath();
-    ctx.moveTo(0,0);
+    ctx.moveTo(event.offsetX, event.offsetY);
+    // isPainting이 true이면(= mousedown 상태가 아니면) 브러시 시작점만 이동
+}
 
-    
-    const color = selectTheme[Math.trunc(Math.random() * selectTheme.length)];
-    ctx.strokeStyle = color;
+function onMouseDown() {
+    isPainting = true;
+}
 
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-};
+function onMouseUp() {
+    isPainting = false;
+}
 
-canvas.addEventListener('mousemove', onClick);
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+    ctx.strokeStyle = event.target.dataset.color;
+    ctx.fillStyle = event.target.dataset.color;
+    color.value = event.target.dataset.color; // color input의 색상도 바꿔주기
+    currentColor.innerText = event.target.dataset.color;
+}
+
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', onMouseDown);
+canvas.addEventListener('mouseup', onMouseUp);
+canvas.addEventListener('mouseleave', onMouseUp);
+
+lineWidth.addEventListener('change', onLineWidthChange);
+color.addEventListener('change',onColorChange);
+
+colorOptions.forEach(color => color.addEventListener('click', onColorClick));
